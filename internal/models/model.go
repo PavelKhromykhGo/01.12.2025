d7c0221e5899ad662690dad20928afc063c327ba
PavelKhromykh <drykilllogic232a@gmail.com>
2025-11-29 16:40:07 +0300

    add models

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,1 +1,18 @@
 package models
+
+type LinkStatus string
+
+const (
+	StatusAvailable    LinkStatus = "available"
+	StatusNotAvailable LinkStatus = "not available"
+)
+
+type LinkCheck struct {
+	URL    string `json:"url"`
+	Status string `json:"status"`
+}
+
+type ListGroup struct {
+	ID    int         `json:"id"`
+	Links []LinkCheck `json:"links"`
+}
